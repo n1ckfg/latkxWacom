eed0932396d30866900403c4faaa66e9274195df
n1ckfg <nick@fox-gieg.com>
2019-02-17 12:44:45 -0500

    improve timeout

--- Scripts/WacomOffset.cs
+++ Scripts/WacomOffset.cs
@@ -6,22 +6,27 @@ using PressureLib;
 public class WacomOffset : MonoBehaviour
 {
 
+    public LightningArtist latk;
     public SteamVR_NewController mainCtl;
     public SteamVR_NewController altCtl;
     public Renderer wacomGrid;
     public Vector2 wacomCursor = Vector2.zero;
     public bool isActive = false;
-    public float triggerDistance = 0.0001f;
-    public float timeout = 2f;
+    public float triggerDistance = 0.001f;
+    public float timeout = 10f;
 
     private Vector3 origPos;
     private Quaternion origRot;
     private Vector2 lastCursor = Vector2.zero;
+    private float lastCursorDist = 0f;
     private float sensitivityX = 2f;
     private float sensitivityY = 2f;
+    private bool blockRetrigger = false;
+    private float retriggerTimeout = 5f;
+    private float timeoutCounter = 0f;
 
     private void Awake() {
-        wacomGrid.enabled = false;
+        if (!latk.useCollisions) wacomGrid.enabled = false;
 
         // for some reason sensitivity is greater in build than in editor
 #if !UNITY_EDITOR
@@ -34,47 +39,73 @@ public class WacomOffset : MonoBehaviour
     private void Start() {
         origPos = transform.localPosition;
         origRot = transform.localRotation;
-        //StartCoroutine(startTimeout());
+
+        wacomCursorUpdate();
     }
 
     private void Update() {
-        wacomCursor = new Vector2(PressureManager.cursorPosition.x / Screen.width, (PressureManager.cursorPosition.y / Screen.height) - 0.5f);
+        wacomCursorUpdate();
 
-        //if (Time.realtimeSinceStartup > timeout) {
-            if (Input.GetMouseButtonDown(1)) {
-                if (!isActive) {
-                    isActive = true;
-                    wacomGrid.enabled = true;
-                    transform.parent = altCtl.transform;
-                    transform.localPosition = origPos;
-                    transform.localRotation = origRot;
-                } else {
-                    isActive = false;
-                    wacomGrid.enabled = false;
-                    transform.parent = mainCtl.transform;
-                    transform.localPosition = origPos;
-                    transform.localRotation = origRot;
-                }
+        if (Input.GetMouseButton(0)) timeoutCounter = 0;
+
+        if (Input.GetMouseButtonDown(1)) {
+            if (!isActive) {
+                wacomModeStart();
+            } else {
+                wacomModeEnd();
             }
+        }
 
-            if (isActive) {
-                wacomGrid.enabled = true;
-                transform.localPosition = new Vector3(wacomCursor.x, transform.localPosition.y, wacomCursor.y);
-                mainCtl.triggerPressed = Input.GetMouseButton(0);
+        if (lastCursorDist >= triggerDistance) {
+            timeoutCounter = 0f;
+            if (!isActive && !blockRetrigger) {
+                wacomModeStart();
             }
-        //}
+        } else {
+            timeoutCounter += Time.deltaTime;
+            if (timeoutCounter > timeout) wacomModeEnd();
+        }
 
-        //lastCursor = new Vector2(wacomCursor.x, wacomCursor.y);
-    }
-
-    /*
-    private IEnumerator startTimeout() {
-        while (true) {
-            yield return new WaitForSeconds(timeout);
-            if (Vector2.Distance(wacomCursor, lastCursor) >= triggerDistance) isActive = true;
+        if (isActive) {
+            wacomModeUpdate();
         }
     }
-    */
+
+    void wacomCursorUpdate() {
+        lastCursor = new Vector2(wacomCursor.x, wacomCursor.y);
+        wacomCursor = new Vector2(PressureManager.cursorPosition.x / Screen.width, (PressureManager.cursorPosition.y / Screen.height) - 0.5f);
+        lastCursorDist = Vector2.Distance(wacomCursor, lastCursor);
+    }
+
+    void wacomModeStart() {
+        timeoutCounter = 0f;
+        isActive = true;
+        wacomGrid.enabled = true;
+        transform.parent = altCtl.transform;
+        transform.localPosition = origPos;
+        transform.localRotation = origRot;
+    }
+
+    void wacomModeEnd() {
+        timeoutCounter = 0f;
+        isActive = false;
+        if (!latk.useCollisions) wacomGrid.enabled = false;
+        transform.parent = mainCtl.transform;
+        transform.localPosition = origPos;
+        transform.localRotation = origRot;
+        StartCoroutine(startTimeout());
+    }
+
+    void wacomModeUpdate() {
+        transform.localPosition = new Vector3(wacomCursor.x, transform.localPosition.y, wacomCursor.y);
+        mainCtl.triggerPressed = Input.GetMouseButton(0);
+    }
+
+    private IEnumerator startTimeout() {
+        blockRetrigger = true;
+        yield return new WaitForSeconds(retriggerTimeout);
+        blockRetrigger = false;
+    }
 
 }
  
