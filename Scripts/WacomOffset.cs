7f487aa3b8aeb2ff48263af8bf58a0e73507eef0
n1ckfg <nick@fox-gieg.com>
2019-02-17 18:52:51 -0500

    tweak mapping

--- Scripts/WacomOffset.cs
+++ Scripts/WacomOffset.cs
@@ -24,6 +24,8 @@ public class WacomOffset : MonoBehaviour
     private bool blockRetrigger = false;
     private float retriggerTimeout = 5f;
     private float timeoutCounter = 0f;
+    private Vector2 posOffset = new Vector2(-0.5f, -0.5f);
+    private float gridOffset = 0.36f;
 
     private void Awake() {
         if (!latk.useCollisions) wacomGrid.enabled = false;
@@ -37,8 +39,12 @@ public class WacomOffset : MonoBehaviour
     }
 
     private void Start() {
+        posOffset += new Vector2(0.025f, 0.025f);
         origPos = transform.localPosition;
         origRot = transform.localRotation;
+
+        posOffset.y += gridOffset/2f;
+        wacomGrid.transform.localPosition = new Vector3(wacomGrid.transform.localPosition.x, wacomGrid.transform.localPosition.y, wacomGrid.transform.localPosition.z + gridOffset);
 
         wacomCursorUpdate();
     }
@@ -48,6 +54,7 @@ public class WacomOffset : MonoBehaviour
 
         if (Input.GetMouseButton(0)) timeoutCounter = 0;
 
+        /*
         if (Input.GetMouseButtonDown(1)) {
             if (!isActive) {
                 wacomModeStart();
@@ -55,6 +62,7 @@ public class WacomOffset : MonoBehaviour
                 wacomModeEnd();
             }
         }
+        */
 
         if (lastCursorDist >= triggerDistance) {
             timeoutCounter = 0f;
@@ -73,7 +81,7 @@ public class WacomOffset : MonoBehaviour
 
     void wacomCursorUpdate() {
         lastCursor = new Vector2(wacomCursor.x, wacomCursor.y);
-        wacomCursor = new Vector2(PressureManager.cursorPosition.x / Screen.width, (PressureManager.cursorPosition.y / Screen.height) - 0.5f);
+        wacomCursor = new Vector2((PressureManager.cursorPosition.x / Screen.width) + posOffset.x, (PressureManager.cursorPosition.y / Screen.width) + posOffset.y + gridOffset);
         lastCursorDist = Vector2.Distance(wacomCursor, lastCursor);
     }
 
@@ -99,7 +107,16 @@ public class WacomOffset : MonoBehaviour
 
     void wacomModeUpdate() {
         transform.localPosition = new Vector3(wacomCursor.x, transform.localPosition.y, wacomCursor.y);
+
+        mainCtl.triggerDown = Input.GetMouseButtonDown(0);
         mainCtl.triggerPressed = Input.GetMouseButton(0);
+        mainCtl.triggerUp = Input.GetMouseButtonUp(0);
+
+        mainCtl.menuDown = Input.GetMouseButtonDown(1);
+        mainCtl.menuPressed = Input.GetMouseButton(1);
+        mainCtl.menuUp = Input.GetMouseButtonUp(1);
+
+        //Debug.Log(wacomCursor.x + " " + wacomCursor.y + " " + mainCtl.triggerPressed);
     }
 
     private IEnumerator startTimeout() {
