e4222f35ca3f3188ddea4615ab0434593cd6a266
Nick Fox-Gieg <nick@fox-gieg.com>
2020-01-11 16:23:31 -0500

    add detach-attach class

---
+++ Scripts/WacomMode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WacomMode : MonoBehaviour {
+
+    private Transform origParent;
+    private Vector3 origPos;
+    private Vector3 origScale;
+    private Quaternion origRot;
+    private bool firstRun;
+
+    private void Start() {
+        origParent = transform.parent;
+        saveTransform();
+    }
+
+    private void saveTransform() {
+        origPos = transform.localPosition;
+        origRot = transform.localRotation;
+        origScale = transform.localScale;
+    }
+
+    private void restoreTransform() {
+        transform.localPosition = origPos;
+        transform.localRotation = origRot;
+        transform.localScale = origScale;
+    }
+
+    public void attach() {
+        transform.SetParent(origParent);
+        restoreTransform();
+    }
+
+    public void detach() {
+        transform.SetParent(null, true);
+    }
+
+}
